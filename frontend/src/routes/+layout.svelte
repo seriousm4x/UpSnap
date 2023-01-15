9a4f29d63d362d39d56963eddfd14151964dfa0f
Maxi Quoß <maxi@quoss.org>
2023-01-15 17:17:18 +0100

    sveltekit init

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Navbar from '@components/Navbar.svelte';
+</script>
+
+<Navbar />
+
+<slot />
+
+<style lang="scss" global>
+    @import '../scss/main.scss';
+</style>
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import PocketBase from 'pocketbase';
+	import { onMount } from 'svelte';
+
+	let devices = [];
+
+	onMount(async () => {
+		const pb = new PocketBase('http://127.0.0.1:8090');
+		const result = await pb.collection('devices').getFullList();
+		devices = result;
+	});
+</script>
+
+{#each devices as device}
+	{device.name}
+{/each}
