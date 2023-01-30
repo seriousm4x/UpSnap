6b4c6b94305d6d36aab1688dd1aba7fbaf3184fb
Maxi Quoß <maxi@quoss.org>
2023-01-30 15:32:52 +0100

    add v3 update notice

--- app/backend/backend/settings.py
+++ app/backend/backend/settings.py
@@ -174,4 +174,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-VERSION = "v2.3.2"
+VERSION = "v2.3.3"
