c37294dec00850f17c8b2d68151e0cc9bf224752
Maxi Quoß <maxi@quoss.org>
2023-06-28 11:50:08 +0200

    update dependencies

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,8 +1,6 @@
 module.exports = {
     root: true,
     extends: ['eslint:recommended', 'prettier'],
-    plugins: ['svelte3'],
-    overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
     parserOptions: {
         sourceType: 'module',
         ecmaVersion: 2020
