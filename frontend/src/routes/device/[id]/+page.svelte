a875613b7693795f6175224eb67166ca04c394b9
seriousm4x <maxi@quoss.org>
2023-07-25 18:19:21 +0200

    finish device edit, fix navbar

--- frontend/src/routes/device/[id]/+page.svelte
+++ frontend/src/routes/device/[id]/+page.svelte
@@ -195,11 +195,11 @@
 					</div>
 				</div>
 				<h2 class="card-title mt-8">Ports</h2>
+				<p class="my-2">
+					UpSnap can also check if given ports are open. You can define them below.
+				</p>
 				<div class="form-control w-full">
-					<p class="my-2">
-						UpSnap can also check if given ports are open. You can define them below.
-					</p>
-					<div class="flex flex-wrap gap-4">
+					<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
 						{#each deviceClone.expand.ports as _, index}
 							<DeviceFormPort bind:deviceClone {index} bind:portErrMsg bind:portErrTimeout />
 						{/each}
@@ -219,15 +219,137 @@
 					>
 				</div>
 				<h2 class="card-title mt-8">Link</h2>
+				<p class="my-2">
+					Makes your device name a clickable link, perfect for linking a dashboard for example.
+				</p>
 				<div class="form-control w-full">
-					<p class="my-2">
-						Makes your device name a clickable link, perfect for linking a dashboard for example.
-					</p>
 					<input
 						type="url"
 						placeholder="https:// ..."
 						class="input w-full max-w-xs"
 						bind:value={deviceClone.link}
+					/>
+				</div>
+				<h2 class="card-title mt-8">Wake</h2>
+				<p class="my-2">
+					You can power this device using a scheduled cron job. If you are not familiar with cron,
+					you can read about it <a
+						class="link"
+						href="https://en.wikipedia.org/wiki/Cron"
+						target="_blank">here</a
+					>
+					or refer to the
+					<a class="link" href="https://pkg.go.dev/github.com/robfig/cron/v3" target="_blank"
+						>package documentation</a
+					>
+					for more information. <strong>Tip:</strong> Once you've entered your syntax into the input
+					field and click out of it, your syntax will be automatically validated to check for correctness.
+				</p>
+				<div class="form-control flex flex-row flex-wrap gap-4">
+					<div>
+						<label class="label" for="wake-cron">
+							<span class="label-text">Wake cron</span>
+						</label>
+						<input
+							id="wake-cron"
+							type="text"
+							placeholder="0 9 * * 1-5"
+							class="input w-80"
+							bind:value={deviceClone.wake_cron}
+							disabled={!deviceClone.wake_cron_enabled}
+						/>
+					</div>
+					<div class="flex flex-col">
+						<label class="label cursor-pointer" for="wake-cron-enable">
+							<span class="label-text">Enable wake cron</span>
+						</label>
+						<input
+							id="wake-cron-enable"
+							type="checkbox"
+							class="toggle toggle-success"
+							bind:checked={deviceClone.wake_cron_enabled}
+						/>
+					</div>
+				</div>
+				<h2 class="card-title mt-8">Shutdown</h2>
+				<p class="my-2">
+					This <strong>shell command</strong> will run inside your container (if you use docker) or
+					on your host (if you use the binary). To verify that works you can run the command inside
+					the container or on your host shell first. Common commands are
+					<span class="badge">net rpc</span> for windows, <span class="badge">sshpass</span> for
+					linux or
+					<span class="badge">curl</span> in general to make web requests.
+				</p>
+				<p class="my-2 font-bold">Example:</p>
+				<div class="alert text-sm bg-base-100 text-start">
+					<div>
+						<span class="font-bold">Shutdown remote windows machine:</span>
+						<pre
+							class="break-words whitespace-pre-wrap">net rpc shutdown -I 192.168.1.13 -U "user%password"</pre>
+					</div>
+				</div>
+				<div class="alert text-sm my-2 bg-base-100 text-start">
+					<div>
+						<span class="font-bold">Shutdown remote linux machine:</span>
+						<pre
+							class="break-words whitespace-pre-wrap">sshpass -p your_password ssh -o "StrictHostKeyChecking=no" user@hostname "sudo poweroff"</pre>
+					</div>
+				</div>
+				<div class="form-control w-full">
+					<label class="label cursor-pointer" for="shutdown-cmd">
+						<span class="label-text">Shutdown command</span>
+					</label>
+					<input
+						id="shutdown-cmd"
+						type="text"
+						placeholder="$:"
+						class="input w-full max-w-xs mb-2"
+						bind:value={deviceClone.shutdown_cmd}
+					/>
+				</div>
+				<p class="my-2">
+					Just like setting a cron to wake the device, you can also schedule a cron job to shut down
+					this device.
+				</p>
+				<div class="form-control flex flex-row flex-wrap gap-4">
+					<div>
+						<label class="label" for="shutdown-cron">
+							<span class="label-text">Shutdown cron</span>
+						</label>
+						<input
+							id="shutdown-cron"
+							type="text"
+							placeholder="0 22 * * 1-5"
+							class="input w-80"
+							bind:value={deviceClone.shutdown_cron}
+							disabled={!deviceClone.shutdown_cron_enabled}
+						/>
+					</div>
+					<div class="flex flex-col">
+						<label class="label cursor-pointer" for="shutdown-cron-enable">
+							<span class="label-text">Enable shutdown cron</span>
+						</label>
+						<input
+							id="shutdown-cron-enable"
+							type="checkbox"
+							class="toggle toggle-success"
+							bind:checked={deviceClone.shutdown_cron_enabled}
+						/>
+					</div>
+				</div>
+				<h2 class="card-title mt-8">Password</h2>
+				<p class="my-2">
+					Some network cards have the option to set a password for magic packets, also called <span
+						class="badge">SecureON</span
+					>. Password can only be 0, 4 or 6 characters in length.
+				</p>
+				<div class="form-control w-full">
+					<input
+						type="password"
+						placeholder="123456"
+						class="input w-full max-w-xs"
+						maxlength="6"
+						bind:value={deviceClone.password}
 					/>
 				</div>
 				{#if saveErrMsg !== ''}
