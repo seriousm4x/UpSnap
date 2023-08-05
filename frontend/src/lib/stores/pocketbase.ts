b6db86513dc2fa4ca2fa70354a001cbea9f10d3c
seriousm4x <maxi@quoss.org>
2023-08-05 14:08:26 +0200

    improve home loading, remove $devices store

--- frontend/src/lib/stores/pocketbase.ts
+++ frontend/src/lib/stores/pocketbase.ts
@@ -1,6 +1,5 @@
 import { writable } from 'svelte/store';
 import PocketBase from 'pocketbase';
-import type { Device } from '$lib/types/device';
 
 // set backend url based on environment
 export const backendUrl = import.meta.env.DEV ? 'http://127.0.0.1:8090/' : '/';
@@ -11,4 +10,3 @@ pb.autoCancellation(false);
 
 // export stores
 export const pocketbase = writable(pb);
-export const devices = writable<Device[]>([]);
