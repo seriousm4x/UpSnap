870d2dcc49550facf5681c7d3978de284a0cac39
seriousm4x <maxi@quoss.org>
2023-08-13 20:00:13 +0200

    improve /login and /welcome

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { pocketbase, isAdmin } from '$lib/stores/pocketbase';
+	import { settingsPub } from '$lib/stores/settings';
 	import Fa from 'svelte-fa';
 	import { faLockOpen, faEye } from '@fortawesome/free-solid-svg-icons';
 	import { toggleVisibility } from '$lib/helpers/forms';
@@ -34,54 +35,53 @@
 	}
 </script>
 
-<div class="flex h-screen">
-	<div class="m-auto">
-		<div class="flex flex-col gap-16 w-screen max-w-lg my-4">
-			<div class="card bg-base-300 shadow-xl">
-				<div class="card-body">
-					<div class="flex flex-row gap-4">
-						<figure class="w-16"><img src="/gopher.svg" alt="Gopher" /></figure>
-						<h2 class="card-title">Welcome</h2>
-					</div>
-					<form class="form-control w-full" on:submit|preventDefault={tryAdminThenUser}>
-						<label class="label" for="email">
-							<span class="label-text">Email or Username:</span>
-						</label>
-						<input
-							id="email"
-							type="text"
-							class="input input-bordered w-full"
-							required
-							bind:value={form.email}
-						/>
-						<label class="label" for="password">
-							<span class="label-text">Password:</span>
-						</label>
-						<label class="relative block">
-							<div
-								class="absolute top-1/2 -translate-y-1/2 right-4 cursor-pointer"
-								role="none"
-								on:click={() => toggleVisibility(inputPassword)}
-								on:keydown={() => toggleVisibility(inputPassword)}
-							>
-								<Fa icon={faEye} />
-							</div>
-							<input
-								id="password"
-								type="password"
-								class="input input-bordered w-full"
-								maxlength="72"
-								required
-								bind:value={form.password}
-								bind:this={inputPassword}
-							/>
-						</label>
-						<div class="card-actions justify-end mt-4">
-							<button class="btn btn-primary" type="submit">Login <Fa icon={faLockOpen} /></button>
-						</div>
-					</form>
-				</div>
+<div class="mt-10 flex items-center justify-center">
+	<div class="card bg-base-300 shadow-xl w-full max-w-lg">
+		<div class="card-body">
+			{#if $settingsPub?.website_title}
+				<h1 class="text-xl text-center font-bold">{$settingsPub?.website_title}</h1>
+			{/if}
+			<div class="flex flex-row gap-4">
+				<figure class="w-16"><img src="/gopher.svg" alt="Gopher" /></figure>
+				<h2 class="card-title">Welcome</h2>
 			</div>
+			<form class="form-control w-full" on:submit|preventDefault={tryAdminThenUser}>
+				<label class="label" for="email">
+					<span class="label-text">Email or Username:</span>
+				</label>
+				<input
+					id="email"
+					type="text"
+					class="input input-bordered w-full"
+					required
+					bind:value={form.email}
+				/>
+				<label class="label" for="password">
+					<span class="label-text">Password:</span>
+				</label>
+				<label class="relative block">
+					<div
+						class="absolute top-1/2 -translate-y-1/2 right-4 cursor-pointer"
+						role="none"
+						on:click={() => toggleVisibility(inputPassword)}
+						on:keydown={() => toggleVisibility(inputPassword)}
+					>
+						<Fa icon={faEye} />
+					</div>
+					<input
+						id="password"
+						type="password"
+						class="input input-bordered w-full"
+						maxlength="72"
+						required
+						bind:value={form.password}
+						bind:this={inputPassword}
+					/>
+				</label>
+				<div class="card-actions justify-end mt-4">
+					<button class="btn btn-primary" type="submit">Login <Fa icon={faLockOpen} /></button>
+				</div>
+			</form>
 		</div>
 	</div>
 </div>
--- frontend/src/routes/welcome/+page.svelte
+++ frontend/src/routes/welcome/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import { pocketbase } from '$lib/stores/pocketbase';
+	import { pocketbase, isAdmin } from '$lib/stores/pocketbase';
 	import { settingsPub } from '$lib/stores/settings';
 	import { toggleVisibility } from '$lib/helpers/forms';
 	import toast from 'svelte-french-toast';
@@ -28,6 +28,7 @@
 					.authWithPassword(form.email, form.password)
 					.then(() => {
 						stepsCompleted = 2;
+						isAdmin.set(true);
 					})
 					.catch((err) => {
 						if (err.data?.data?.identity?.message) {
@@ -48,119 +49,117 @@
 	}
 </script>
 
-<div class="flex h-screen">
-	<div class="m-auto">
-		<div class="flex flex-col gap-16 w-screen max-w-lg my-4">
-			<div class="card bg-base-300 shadow-xl">
-				{#if $settingsPub?.setup_completed}
-					<figure class="w-72 mx-auto pt-6"><img src="/gopher.svg" alt="Gopher" /></figure>
-					<div class="card-body">
-						<h2 class="card-title">I didn't expect you here! 🧐</h2>
-						<p>You are already done with the setup! Nothing to do.</p>
-						<div class="card-actions justify-end">
-							<button class="btn btn-primary" on:click={() => goto('/')}>Take me back</button>
-						</div>
+<div class="mt-10 flex items-center justify-center">
+	<div class="flex flex-col gap-16 w-screen max-w-lg my-4">
+		<div class="card bg-base-300 shadow-xl">
+			{#if $settingsPub?.setup_completed}
+				<figure class="w-72 mx-auto pt-6"><img src="/gopher.svg" alt="Gopher" /></figure>
+				<div class="card-body">
+					<h2 class="card-title">I didn't expect you here! 🧐</h2>
+					<p>You are already done with the setup! Nothing to do.</p>
+					<div class="card-actions justify-end">
+						<button class="btn btn-primary" on:click={() => goto('/')}>Take me back</button>
 					</div>
-				{:else if stepsCompleted === 0}
-					<figure class="w-72 mx-auto pt-6"><img src="/gopher.svg" alt="Gopher" /></figure>
-					<div class="card-body">
-						<h2 class="card-title">Welcome to UpSnap 🥳</h2>
-						<p>Please complete the following steps to finish the setup.</p>
-						<div class="card-actions justify-end">
-							<button class="btn btn-primary" on:click={() => (stepsCompleted = 1)}
-								>Next <Fa icon={faArrowRight} /></button
+				</div>
+			{:else if stepsCompleted === 0}
+				<figure class="w-44 mx-auto pt-6"><img src="/gopher.svg" alt="Gopher" /></figure>
+				<div class="card-body">
+					<h2 class="card-title">Welcome to UpSnap 🥳</h2>
+					<p>Please complete the following steps to finish the setup.</p>
+					<div class="card-actions justify-end">
+						<button class="btn btn-primary" on:click={() => (stepsCompleted = 1)}
+							>Next <Fa icon={faArrowRight} /></button
+						>
+					</div>
+				</div>
+			{:else if stepsCompleted === 1}
+				<div class="card-body">
+					<div class="flex flex-row gap-4">
+						<figure class="w-16"><img src="/gopher.svg" alt="Gopher" /></figure>
+						<h2 class="card-title">Create an admin account</h2>
+					</div>
+					<form class="form-control w-full" on:submit|preventDefault={register}>
+						<label class="label" for="email">
+							<span class="label-text">Email:</span>
+						</label>
+						<input
+							id="email"
+							type="email"
+							class="input input-bordered w-full"
+							required
+							bind:value={form.email}
+						/>
+						<label class="label" for="password">
+							<span class="label-text">Password:</span>
+							<span class="label-text-alt">min. 10 characters</span>
+						</label>
+						<label class="relative block">
+							<div
+								class="absolute top-1/2 -translate-y-1/2 right-4 cursor-pointer"
+								role="none"
+								on:click={() => toggleVisibility(inputPassword)}
+								on:keydown={() => toggleVisibility(inputPassword)}
+							>
+								<Fa icon={faEye} />
+							</div>
+							<input
+								id="password"
+								type="password"
+								class="input input-bordered w-full"
+								minlength="10"
+								maxlength="72"
+								required
+								bind:value={form.password}
+								bind:this={inputPassword}
+							/>
+						</label>
+						<label class="label" for="passwordConfirm">
+							<span class="label-text">Password confirm:</span>
+						</label>
+						<label class="relative block">
+							<div
+								class="absolute top-1/2 -translate-y-1/2 right-4 cursor-pointer"
+								role="none"
+								on:click={() => toggleVisibility(inputConfirm)}
+								on:keydown={() => toggleVisibility(inputConfirm)}
+							>
+								<Fa icon={faEye} />
+							</div>
+							<input
+								id="confirm"
+								type="password"
+								class="input input-bordered w-full"
+								minlength="10"
+								maxlength="72"
+								required
+								bind:value={form.confirm}
+								bind:this={inputConfirm}
+							/>
+						</label>
+						<div class="card-actions justify-end mt-4">
+							<button class="btn btn-primary" type="submit"
+								>Create <Fa icon={faArrowRight} /></button
 							>
 						</div>
+					</form>
+				</div>
+			{:else if stepsCompleted === 2}
+				<figure class="w-72 mx-auto pt-6"><img src="/gopher.svg" alt="Gopher" /></figure>
+				<div class="card-body">
+					<h2 class="card-title">You are all set! 🎉</h2>
+					<p>Go ahead and add some devices to your dashboard.</p>
+					<div class="card-actions justify-end">
+						<button class="btn btn-success" on:click={() => goto('/')}>Lets go!</button>
 					</div>
-				{:else if stepsCompleted === 1}
-					<div class="card-body">
-						<div class="flex flex-row gap-4">
-							<figure class="w-16"><img src="/gopher.svg" alt="Gopher" /></figure>
-							<h2 class="card-title">Create an admin account</h2>
-						</div>
-						<form class="form-control w-full" on:submit|preventDefault={register}>
-							<label class="label" for="email">
-								<span class="label-text">Email:</span>
-							</label>
-							<input
-								id="email"
-								type="email"
-								class="input input-bordered w-full"
-								required
-								bind:value={form.email}
-							/>
-							<label class="label" for="password">
-								<span class="label-text">Password:</span>
-								<span class="label-text-alt">min. 10 characters</span>
-							</label>
-							<label class="relative block">
-								<div
-									class="absolute top-1/2 -translate-y-1/2 right-4 cursor-pointer"
-									role="none"
-									on:click={() => toggleVisibility(inputPassword)}
-									on:keydown={() => toggleVisibility(inputPassword)}
-								>
-									<Fa icon={faEye} />
-								</div>
-								<input
-									id="password"
-									type="password"
-									class="input input-bordered w-full"
-									minlength="10"
-									maxlength="72"
-									required
-									bind:value={form.password}
-									bind:this={inputPassword}
-								/>
-							</label>
-							<label class="label" for="passwordConfirm">
-								<span class="label-text">Password confirm:</span>
-							</label>
-							<label class="relative block">
-								<div
-									class="absolute top-1/2 -translate-y-1/2 right-4 cursor-pointer"
-									role="none"
-									on:click={() => toggleVisibility(inputConfirm)}
-									on:keydown={() => toggleVisibility(inputConfirm)}
-								>
-									<Fa icon={faEye} />
-								</div>
-								<input
-									id="confirm"
-									type="password"
-									class="input input-bordered w-full"
-									minlength="10"
-									maxlength="72"
-									required
-									bind:value={form.confirm}
-									bind:this={inputConfirm}
-								/>
-							</label>
-							<div class="card-actions justify-end mt-4">
-								<button class="btn btn-primary" type="submit"
-									>Create <Fa icon={faArrowRight} /></button
-								>
-							</div>
-						</form>
-					</div>
-				{:else if stepsCompleted === 2}
-					<figure class="w-72 mx-auto pt-6"><img src="/gopher.svg" alt="Gopher" /></figure>
-					<div class="card-body">
-						<h2 class="card-title">You are all set! 🎉</h2>
-						<p>Go ahead and add some devices to your dashboard.</p>
-						<div class="card-actions justify-end">
-							<button class="btn btn-success" on:click={() => goto('/')}>Lets go!</button>
-						</div>
-					</div>
-				{/if}
-			</div>
-			{#if !$settingsPub?.setup_completed}
-				<ul class="steps steps-horizontal">
-					<li class="step step-primary">Welcome</li>
-					<li class="step" class:step-primary={stepsCompleted > 0}>Create account</li>
-					<li class="step" class:step-primary={stepsCompleted > 1}>Done</li>
-				</ul>
+				</div>
 			{/if}
 		</div>
+		{#if !$settingsPub?.setup_completed}
+			<ul class="steps steps-horizontal">
+				<li class="step step-primary">Welcome</li>
+				<li class="step" class:step-primary={stepsCompleted > 0}>Create account</li>
+				<li class="step" class:step-primary={stepsCompleted > 1}>Done</li>
+			</ul>
+		{/if}
 	</div>
 </div>
