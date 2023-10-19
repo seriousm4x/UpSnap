86ed3c1c08a728628bbfb0b8063215f961705036
seriousm4x <maxi@quoss.org>
2023-10-19 19:34:18 +0200

    fix: cross platform build

--- backend/networking/shutdown_other.go
+++ backend/networking/shutdown_other.go
@@ -1,5 +1,5 @@
-//go:build windows
-// +build windows
+//go:build !windows
+// +build !windows
 
 package networking
 
@@ -7,13 +7,29 @@ import (
 	"os"
 	"os/exec"
 
-	"golang.org/x/sys/windows"
+	"github.com/seriousm4x/upsnap/logger"
+	"golang.org/x/sys/unix"
 )
 
 // Set platform specifiy custom process attributes
-func SetProcessAttributes(cmd *exec.Cmd) {}
+func SetProcessAttributes(cmd *exec.Cmd) {
+	cmd.SysProcAttr = &unix.SysProcAttr{Setpgid: true}
+}
 
 // Kills child processes on Linux. Windows doesn't provide a direct way to kill child processes, so we kill just the main process.
 func KillProcess(process *os.Process) error {
-	return windows.GenerateConsoleCtrlEvent(windows.CTRL_BREAK_EVENT, uint32(process.Pid))
+	logger.Warning.Println("Your shutdown cmd didn't finish in 2 minutes. It will be killed.")
+	pgid, err := unix.Getpgid(process.Pid)
+	if err != nil {
+		return err
+	}
+
+	err = unix.Kill(-pgid, unix.SIGTERM)
+	if err != nil {
+		return unix.Kill(-pgid, unix.SIGKILL)
+	}
+
+	_, err = process.Wait()
+
+	return err
 }
---
+++ backend/networking/shutdown_windows.go
@@ -0,0 +1,19 @@
+//go:build windows
+// +build windows
+
+package networking
+
+import (
+	"os"
+	"os/exec"
+
+	"golang.org/x/sys/windows"
+)
+
+// Set platform specifiy custom process attributes
+func SetProcessAttributes(cmd *exec.Cmd) {}
+
+// Kills child processes on Linux. Windows doesn't provide a direct way to kill child processes, so we kill just the main process.
+func KillProcess(process *os.Process) error {
+	return windows.GenerateConsoleCtrlEvent(windows.CTRL_BREAK_EVENT, uint32(process.Pid))
+}
