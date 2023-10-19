f3ba2489f4443fa7fe50e779a182d485c5bb3a3f
seriousm4x <maxi@quoss.org>
2023-10-19 11:03:17 +0200

    fix: kill child processes of shutdown cmd #266

--- backend/go.mod
+++ backend/go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/pocketbase/pocketbase v0.19.0
 	github.com/prometheus-community/pro-bing v0.3.0
 	github.com/robfig/cron/v3 v3.0.1
+	golang.org/x/sys v0.13.0
 )
 
 require (
@@ -75,7 +76,6 @@ require (
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/oauth2 v0.13.0 // indirect
 	golang.org/x/sync v0.4.0 // indirect
-	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/term v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
--- backend/networking/shutdown.go
+++ backend/networking/shutdown.go
@@ -4,9 +4,12 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"os"
 	"os/exec"
 	"runtime"
 	"time"
+
+	"golang.org/x/sys/unix"
 
 	"github.com/pocketbase/pocketbase/models"
 	"github.com/seriousm4x/upsnap/logger"
@@ -31,8 +34,14 @@ func ShutdownDevice(device *models.Record) error {
 
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
-
 	cmd := exec.CommandContext(ctx, shell, shell_arg, shutdown_cmd)
+
+	if runtime.GOOS == "linux" {
+		// set group id for child processes, windows doesn't support this
+		// used to avoid zombie processes
+		cmd.SysProcAttr = &unix.SysProcAttr{Setpgid: true}
+	}
+
 	var stderr bytes.Buffer
 	cmd.Stderr = &stderr
 
@@ -51,19 +60,45 @@ func ShutdownDevice(device *models.Record) error {
 		select {
 		case <-time.After(1 * time.Second):
 			if time.Since(start) >= 2*time.Minute {
+				if err := killProcess(cmd.Process); err != nil {
+					logger.Error.Println(err)
+				}
 				cancel()
 				return fmt.Errorf("%s not offline after 2 minutes", device.GetString("name"))
 			}
 			isOnline := PingDevice(device)
 			if !isOnline {
+				if err := killProcess(cmd.Process); err != nil {
+					logger.Error.Println(err)
+				}
 				cancel()
 				return nil
 			}
 		case err := <-done:
 			if err != nil {
+				if err := killProcess(cmd.Process); err != nil {
+					logger.Error.Println(err)
+				}
 				cancel()
 				return fmt.Errorf("%s", stderr.String())
 			}
 		}
+	}
+}
+
+// Kills child processes on Linux. Windows doesn't provide a direct way to kill child processes, so we kill just the main process.
+func killProcess(process *os.Process) error {
+	if runtime.GOOS == "linux" {
+		pgid, err := unix.Getpgid(process.Pid)
+		logger.Debug.Println(pgid)
+		if err != nil {
+			return err
+		}
+		return unix.Kill(-pgid, unix.SIGKILL)
+	} else {
+		if err := process.Kill(); err != nil {
+			return err
+		}
+		return nil
 	}
 }
