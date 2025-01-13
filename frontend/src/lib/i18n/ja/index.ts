74326f3436441f81411c5af933c6e11fb37bf5b4
Maxi Quoß <maxi@quoss.org>
2025-01-14 00:04:21 +0100

    prettier formatting

--- frontend/src/lib/i18n/ja/index.ts
+++ frontend/src/lib/i18n/ja/index.ts
@@ -60,9 +60,11 @@ const ja = {
 		ports_name: '名前',
 		ports_number: '番号',
 		link: 'リンク',
-		link_desc: 'デバイス名をクリック可能なリンクにします。たとえばダッシュボードへのリンクに最適です。',
+		link_desc:
+			'デバイス名をクリック可能なリンクにします。たとえばダッシュボードへのリンクに最適です。',
 		ping: 'Ping',
-		ping_desc: 'デバイスがオンになっているか確認するためにカスタムシェルコマンドを使用できます。このコマンドは、デバイスがオンであることを示すには終了コード <span class="badge">0</span> を返す必要があります。その他の終了コードはオフとして扱われます。',
+		ping_desc:
+			'デバイスがオンになっているか確認するためにカスタムシェルコマンドを使用できます。このコマンドは、デバイスがオンであることを示すには終了コード <span class="badge">0</span> を返す必要があります。その他の終了コードはオフとして扱われます。',
 		ping_cmd: 'カスタム Ping コマンド',
 		wake: '起動',
 		wake_desc: 'スケジュールされた cron ジョブを使用してこのデバイスを起動できます。',
@@ -70,30 +72,38 @@ const ja = {
 		wake_cron: '起動スケジュール',
 		wake_cron_enable: '起動スケジュールを有効化',
 		sol: 'Sleep-On-LAN',
-		sol_desc1: '<a class="link" href="https://github.com/SR-G/sleep-on-lan" target="_blank">Sleep-On-LAN</a> ツールを使用してコンピュータをスリープ状態にすることができます。Sleep-On-LAN（SOL）は、スリープ状態にしたい PC で動作する外部ツール/デーモンで、REST エンドポイントを提供します。Sleep-On-LAN のセットアップ方法については、<a href="https://github.com/SR-G/sleep-on-lan#usage" class="link" target="_blank">Usage</a> セクションを参照してください。',
-		sol_desc2: 'SOL は、認証を有効にし、要求をより信頼性の高いものにするために、UDP の代わりに HTTP を使用してリクエストを送信するように構成されています。',
-		sol_desc3: 'したがって、<a href="https://github.com/SR-G/sleep-on-lan#configuration" class="link" target="_blank">SOL 構成</a>の <span class="badge">Listeners</span> セクションに <span class="badge">HTTP:&lt;YOURPORT&gt;</span> を含めるようにしてください。',
+		sol_desc1:
+			'<a class="link" href="https://github.com/SR-G/sleep-on-lan" target="_blank">Sleep-On-LAN</a> ツールを使用してコンピュータをスリープ状態にすることができます。Sleep-On-LAN（SOL）は、スリープ状態にしたい PC で動作する外部ツール/デーモンで、REST エンドポイントを提供します。Sleep-On-LAN のセットアップ方法については、<a href="https://github.com/SR-G/sleep-on-lan#usage" class="link" target="_blank">Usage</a> セクションを参照してください。',
+		sol_desc2:
+			'SOL は、認証を有効にし、要求をより信頼性の高いものにするために、UDP の代わりに HTTP を使用してリクエストを送信するように構成されています。',
+		sol_desc3:
+			'したがって、<a href="https://github.com/SR-G/sleep-on-lan#configuration" class="link" target="_blank">SOL 構成</a>の <span class="badge">Listeners</span> セクションに <span class="badge">HTTP:&lt;YOURPORT&gt;</span> を含めるようにしてください。',
 		sol_enable: 'Sleep-On-LAN を有効化',
 		sol_port: 'SOL ポート',
 		sol_authorization: '認証',
 		sol_user: 'SOL ユーザー',
 		sol_password: 'SOL パスワード',
 		shutdown: 'シャットダウン',
-		shutdown_desc: 'この <strong>シェルコマンド</strong> は、コンテナ内（Docker を使用している場合）またはホスト（バイナリを使用している場合）で実行されます。機能を確認するには、まずコマンドをコンテナ内またはホストのシェルで実行してください。一般的なコマンドには、Windows 用の <span class="badge">net rpc</span>、Linux 用の <span class="badge">sshpass</span>、またはウェブリクエストを行うための <span class="badge">curl</span> などがあります。',
+		shutdown_desc:
+			'この <strong>シェルコマンド</strong> は、コンテナ内（Docker を使用している場合）またはホスト（バイナリを使用している場合）で実行されます。機能を確認するには、まずコマンドをコンテナ内またはホストのシェルで実行してください。一般的なコマンドには、Windows 用の <span class="badge">net rpc</span>、Linux 用の <span class="badge">sshpass</span>、またはウェブリクエストを行うための <span class="badge">curl</span> などがあります。',
 		shutdown_examples: '例:',
 		shutdown_examples_windows: 'リモート Windows マシンのシャットダウン:',
 		shutdown_examples_linux: 'リモート Linux マシンのシャットダウン:',
 		shutdown_cmd: 'シャットダウンコマンド',
-		shutdown_cron_desc: 'デバイスを起動するために cron を設定するのと同様に、このデバイスをシャットダウンするための cron ジョブをスケジュールすることもできます。',
+		shutdown_cron_desc:
+			'デバイスを起動するために cron を設定するのと同様に、このデバイスをシャットダウンするための cron ジョブをスケジュールすることもできます。',
 		shutdown_cron: 'シャットダウンスケジュール',
 		shutdown_cron_enable: 'シャットダウンスケジュールを有効化',
 		password: 'パスワード',
-		password_desc: '一部のネットワークカードには、マジックパケット用のパスワード（<span class="badge">SecureON</span> とも呼ばれる）を設定するオプションがあります。パスワードは 0、4、または 6 文字のみの長さである必要があります。',
+		password_desc:
+			'一部のネットワークカードには、マジックパケット用のパスワード（<span class="badge">SecureON</span> とも呼ばれる）を設定するオプションがあります。パスワードは 0、4、または 6 文字のみの長さである必要があります。',
 		groups: 'グループ',
-		groups_desc: 'デバイスをグループに追加することで、ダッシュボード上でグループごとに並べ替えることができます。',
+		groups_desc:
+			'デバイスをグループに追加することで、ダッシュボード上でグループごとに並べ替えることができます。',
 		groups_placeholder: "例: '自宅' や 'オフィス' など",
 		network_scan_range_saved: 'スキャン範囲が保存されました',
-		network_scan_desc: 'ネットワーク上のデバイスを自動的にスキャンします。この機能を使用するには、UpSnap を root/admin 権限で実行し、nmap が $PATH にインストールされて利用可能である必要があります（Docker ユーザーの場合、すでに設定済みで何もする必要はありません）。スキャンには数秒かかる場合があります。',
+		network_scan_desc:
+			'ネットワーク上のデバイスを自動的にスキャンします。この機能を使用するには、UpSnap を root/admin 権限で実行し、nmap が $PATH にインストールされて利用可能である必要があります（Docker ユーザーの場合、すでに設定済みで何もする必要はありません）。スキャンには数秒かかる場合があります。',
 		network_scan_ip_range: 'IP 範囲',
 		network_scan_no_range: 'スキャン範囲なし',
 		network_scan_unsaved_changes: '未保存の変更',
@@ -136,7 +146,8 @@ const ja = {
 	},
 	users: {
 		page_title: 'ユーザー',
-		allow_create_devices: '{username} が新しいデバイスを作成し、デバイスグループを編集できるようにする',
+		allow_create_devices:
+			'{username} が新しいデバイスを作成し、デバイスグループを編集できるようにする',
 		device_permissions: 'デバイスの権限',
 		create_new_device: '新しいデバイスを作成',
 		read: '読み取り',
