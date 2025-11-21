747859d05bbc100c4dd31b039ebb8e5f1ca176ec
Pavel Pikta <devops@pavelpikta.com>
2025-11-21 13:14:02 +0300

    feat: add crossorigin attribute to manifest link (#1503)
    Signed-off-by: Pavel Pikta <pavel_pikta@epam.com>

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -86,7 +86,7 @@
 </script>
 
 <svelte:head>
-	<link rel="manifest" href={backendUrl + 'api/upsnap/manifest.webmanifest'} />
+	<link rel="manifest" href={backendUrl + 'api/upsnap/manifest.webmanifest'} crossorigin="use-credentials" />
 	<link
 		rel="shortcut icon"
 		href={$settingsPub?.id && $settingsPub?.favicon
