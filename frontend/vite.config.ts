66e08ade4ed731f68849ef78cd5b65a5b67dc3d2
Maxi Quoß <maxi@quoss.org>
2026-03-19 14:58:24 +0100

    frontend: use @tailwindcss/vite instead of postcss

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,11 @@
 import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [
+		tailwindcss(),
 		paraglideVitePlugin({ project: './project.inlang', outdir: './src/lib/paraglide' }),
 		sveltekit()
 	]
