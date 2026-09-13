34502d3673a263a603b1bc8c61086077ca301f51
invario <67800603+invario@users.noreply.github.com>
2026-09-13 11:32:19 -0400

    disable IP tracking if field value does not match regex for IPv4
    Signed-off-by: invario <67800603+invario@users.noreply.github.com>

--- frontend/src/lib/components/DeviceForm.svelte
+++ frontend/src/lib/components/DeviceForm.svelte
@@ -22,15 +22,21 @@
 		await getGroups();
 	});
 
+	const fqdnPattern =
+		/^(?:[\p{L}\p{N}](?:[\p{L}\p{N}-]{0,61}[\p{L}\p{N}])?\.)+[\p{L}\p{N}](?:[\p{L}\p{N}-]{0,61}[\p{L}\p{N}])?$/u;
 	const ipPattern =
-		/^(?:(?:[\p{L}\p{N}](?:[\p{L}\p{N}-]{0,61}[\p{L}\p{N}])?\.)+[\p{L}\p{N}](?:[\p{L}\p{N}-]{0,61}[\p{L}\p{N}])?|(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})$/u;
+		/^(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}$/;
 	const macPattern = '^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$';
 	const netmaskPattern =
 		'^(?:255\\.255\\.255\\.(?:255|254|252|248|240|224|192|128|0)|255\\.255\\.(?:255|254|252|248|240|224|192|128|0)\\.0|255\\.(?:255|254|252|248|240|224|192|128|0)\\.0\\.0|(?:255|254|252|248|240|224|192|128|0)\\.0\\.0\\.0)$';
 
+	$: if ($settingsPriv?.track_ip_interval === '' || !ipPattern.test(device.ip)) {
+		device.track_ip = false;
+	}
+
 	async function save() {
 		// validate FQDN/IP
-		if (!ipPattern.test(device.ip)) {
+		if (!(ipPattern.test(device.ip) || fqdnPattern.test(device.ip))) {
 			toast.error(m.settings_invalid_fqdnip());
 			throw new Error('fqdn_ip not valid');
 		}
@@ -292,7 +298,7 @@
 						type="checkbox"
 						bind:checked={device.track_ip}
 						class="toggle toggle-success"
-						disabled={$settingsPriv?.track_ip_interval === ''}
+						disabled={$settingsPriv?.track_ip_interval === '' || !ipPattern.test(device.ip)}
 					/>
 					{m.device_general_track_ip()}
 				</label>
